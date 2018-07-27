1c882685debc4a74019a9f163ed1a8fcd2345035
minesna <admin@veatyou.kro.kr>
2018-07-27 17:47:04 +0900

    Update classic.js

--- Server/lib/Game/games/classic.js
+++ Server/lib/Game/games/classic.js
@@ -100,7 +100,7 @@ exports.getTitle = function(){
 		var i, list = [];
 		var len;
 		
-		/* ºÎÇÏ°¡ ³Ê¹« °É¸°´Ù¸é ÁÖ¼®À» Ç®ÀÚ.
+		/* ë¶€í•˜ê°€ ë„ˆë¬´ ê±¸ë¦°ë‹¤ë©´ ì£¼ì„ì„ í’€ì.
 		R.go(true);
 		return R;
 		*/
@@ -193,8 +193,11 @@ exports.turnEnd = function(){
 	}
 	my.game.late = true;
 	if(target) if(target.game){
+	if(my.opts.outzero) {
+		score = -9999;
+	} else {
 		score = Const.getPenalty(my.game.chain, target.game.score);
-		target.game.score += score;
+	}
 	}
 	getAuto.call(my, my.game.char, my.game.subChar, 0).then(function(w){
 		my.byMaster('turnEnd', {
@@ -412,9 +415,9 @@ function getMission(l){
 }
 function getAuto(char, subc, type){
 	/* type
-		0 ¹«ÀÛÀ§ ´Ü¾î ÇÏ³ª
-		1 Á¸Àç ¿©ºÎ
-		2 ´Ü¾î ¸ñ·Ï
+		0 ë¬´ì‘ìœ„ ë‹¨ì–´ í•˜ë‚˜
+		1 ì¡´ì¬ ì—¬ë¶€
+		2 ë‹¨ì–´ ëª©ë¡
 	*/
 	var my = this;
 	var R = new Lizard.Tail();
@@ -445,7 +448,7 @@ function getAuto(char, subc, type){
 	if(!char){
 		console.log(`Undefined char detected! key=${key} type=${type} adc=${adc}`);
 	}
-	MAN.findOne([ '_id', char || "¡Ú" ]).on(function($mn){
+	MAN.findOne([ '_id', char || "â˜…" ]).on(function($mn){
 		if($mn && bool){
 			if($mn[key] === null) produce();
 			else R.go($mn[key]);
@@ -545,12 +548,12 @@ function getSubChar(char){
 			ca = [ Math.floor(k/28/21), Math.floor(k/28)%21, k%28 ];
 			cb = [ ca[0] + 0x1100, ca[1] + 0x1161, ca[2] + 0x11A7 ];
 			cc = false;
-			if(cb[0] == 4357){ // ¤©¿¡¼­ ¤¤, ¤·
+			if(cb[0] == 4357){ // ã„¹ì—ì„œ ã„´, ã…‡
 				cc = true;
 				if(RIEUL_TO_NIEUN.includes(cb[1])) cb[0] = 4354;
 				else if(RIEUL_TO_IEUNG.includes(cb[1])) cb[0] = 4363;
 				else cc = false;
-			}else if(cb[0] == 4354){ // ¤¤¿¡¼­ ¤·
+			}else if(cb[0] == 4354){ // ã„´ì—ì„œ ã…‡
 				if(NIEUN_TO_IEUNG.indexOf(cb[1]) != -1){
 					cb[0] = 4363;
 					cc = true;
